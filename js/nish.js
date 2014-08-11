839b3e4e34292f28597fc747d0611c7fd9b80a61
Michael Gyarmathy <mgyarmathy@credera.com>
2014-08-10 23:54:27 -0500

    another attempt to fix JSON requests

--- js/nish.js
+++ js/nish.js
@@ -2,7 +2,7 @@ var request = require('browser-request');
 var _ = require('underscore');
 
 module.exports = function() {
-    request({method:'GET', url:'https://raw.githubusercontent.com/mgyarmathy/nish/gh-pages/nish.json', json:true}, function (er, response, body) {
+    request({method:'GET', url:'./nish.json', json:true}, function (er, response, body) {
         document.getElementById('rhyme').innerHTML = "\'"+_.sample(body.rhymesWith)+"\'";
     });
 };
